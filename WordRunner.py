3a49602509d5a2d6d1c6f8bb4bda1bc03a14483f
Nitin Alagu <nervith@macbookpro.home>
2025-10-21 23:31:38 -0400

    Few more rest functions

--- WordRunner.py
+++ WordRunner.py
@@ -1,5 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+import pathlib
 app = FastAPI()
+app = FastAPI(title="Tiny Uploader")
+TEXTS_DIR = pathlib.Path(__file__).parent.resolve() / "texts"
+TEXTS_DIR.mkdir(exist_ok=True)
+
+
 @app.get("/")
 def home():
-    return {"message":"Hello World"}
+    return {"message":"Hello Mother"}
+
+@app.post("/upload")
+async def upload(file: UploadFile = File(...)):
+    dst = TEXTS_DIR / file.filename
+    with dst.open("wb") as f:
+        while True:
+            chunk = await file.read(1024 * 1024)
+            if not chunk:
+                break
+            f.write(chunk)
+    await file.close()
+    return {"saved_to": str(dst)}
+
+@app.get("/start")
+def start():
+    return {"message":"In a hole in the ground, there lived a Hobbit."}
